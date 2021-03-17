26282f667499e89774a46c9b7112d0fce8b3d467
Haolan-Ni <77455717+Haolan-Ni@users.noreply.github.com>
2021-03-17 23:44:57 +0800

    Update lib.py

--- Dwell/DwellML/DwellMl/Ml_models/lib.py
+++ Dwell/DwellML/DwellMl/Ml_models/lib.py
@@ -26,6 +26,9 @@ def Linear_model(data):
     import sklearn.linear_model as sk_linear
     from sklearn.model_selection import cross_val_score
     import matplotlib.pyplot as plt
+    import warnings
+
+    warnings.filterwarnings('ignore')
 
     df = data
     df = df.fillna(0)
@@ -80,6 +83,9 @@ def Descion_Tree_model(data):
     import sklearn.linear_model as sk_linear
     from sklearn.model_selection import cross_val_score
     import matplotlib.pyplot as plt
+    import warnings
+
+    warnings.filterwarnings('ignore')    
 
     df = data
     df = df.fillna(0)
@@ -113,5 +119,70 @@ def Descion_Tree_model(data):
     scores  = cross_val_score(Descion_Tree_model, X_test, y_test, cv=10).mean()
     print("Cross_Val_Score: {}".format(scores))
     
+    
+    
+def KNN_Class_model(data):
+    import pandas as pd
+    import lightgbm as lgb
+    from sklearn.metrics import mean_squared_error
+    from sklearn.datasets import load_iris
+    from sklearn.model_selection import train_test_split
+    import sklearn.linear_model as sk_linear
+    from sklearn.model_selection import cross_val_score
+    import matplotlib.pyplot as plt
+    from sklearn import neighbors
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.model_selection import train_test_split
+    import warnings
+
+    warnings.filterwarnings('ignore')
+    
+    param_grid=[{
+    "weights":["uniform"],
+    "n_neighbors":[i for i in range(1,11)]
+    },
+    {"weights":["distance"],
+    "n_neighbors":[i for i in range(1,11)],
+    "p":[i for i in range(1,6)]
+    }]
+
+    knn_clf = KNeighborsClassifier()
+    grid_search = GridSearchCV(knn_clf,param_grid)
+
+    df = data
+    df = df.fillna(0)
+    feature = ['Device_ID','Category_Num','Gender_Num','Geohash_Num','Start_time_Cat','End_time_Cat','Start_Time_Weight','End_Time_Weight','Duration','Weekday','Week_Num','Month_Num','tavg','weather']
+    X = df[feature]
+    y = df['Next_Geohash_Num']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    grid_search=GridSearchCV(knn_clf,param_grid,n_jobs=-1,verbose=2, cv = 10)
+    grid_search.fit(X_train,y_train)
+    
+    
+    KNN_Class_model = neighbors.KNeighborsClassifier(n_neighbors=grid_search.best_params_['n_neighbors'], p=grid_search.best_params_['p'], weights=grid_search.best_params_['weights'])
+    KNN_Class_model.fit(X_train,y_train)
+    acc7=KNN_Class_model.score(X_test,y_test)
+    
+    
+    print('KNN_Class_model:')
+    print('KNN_Class_model Accuracy:',acc7)
+    
+    y_pred = KNN_Class_model.predict(X_test)
+    
+    plt.figure(facecolor='w')  
+    plt.plot(range(len(y_pred)),y_test,'r-',linewidth=2,label='test')
+    plt.plot(range(len(y_pred)),y_pred,'g-',linewidth=2,label='predict')
+    plt.legend(loc='upper left') 
+    plt.grid(True)  
+    plt.show()
+  
+    print("Cross Validation:")
+    scores  = cross_val_score(KNN_Class_model, X_test, y_test, cv=10).mean()
+    print("Cross_Val_Score: {}".format(scores))
+    
+
     
 
