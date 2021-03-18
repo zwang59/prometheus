a283474ad03f4fe8a67f20b357a68e4771a24a1a
Haolan-Ni <77455717+Haolan-Ni@users.noreply.github.com>
2021-03-18 20:45:45 +0800

    Update lib.py

--- Dwell/DwellML/DwellMl/ANN_models/lib.py
+++ Dwell/DwellML/DwellMl/ANN_models/lib.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 """
-    Library functions for ML models
+    Library functions for ANN models
 """
-def Linear_model(data):
-    
+def ANN_model(data):
     """ 
-    This function performs linear regression on the loaded data.
+    This function performs ANN_model on the loaded data.
 
     Parameters
     ----------
@@ -18,48 +14,76 @@ def Linear_model(data):
 
     Returns
     -------
-    linear regression scores
+    ANN_model scores
 
     """
     import pandas as pd
-    import lightgbm as lgb
-    from sklearn.metrics import mean_squared_error
-    from sklearn.datasets import load_iris
+    from sklearn import metrics
+    import matplotlib.pyplot as plt
+    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.model_selection import GridSearchCV
     from sklearn.model_selection import train_test_split
-    import sklearn.linear_model as sk_linear
+    from sklearn.metrics import mean_squared_error
     from sklearn.model_selection import cross_val_score
     import matplotlib.pyplot as plt
-
-    df = pd.read_csv(data)
-    df = df.fillna(0)
+    
+    df = data
     feature = ['Device_ID','Category_Num','Gender_Num','Geohash_Num','Start_time_Cat','End_time_Cat','Start_Time_Weight','End_Time_Weight','Duration','Weekday','Week_Num','Month_Num','tavg','weather']
     X = df[feature]
     y = df['Next_Geohash_Num']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    print("Train data length:", len(X_train))
-    print("Test data length:", len(X_test))
     
-    Linear_model = sk_linear.LinearRegression(fit_intercept=True,normalize=False,copy_X=True,n_jobs=1,)
-    Linear_model.fit(X_train,y_train)
-    acc=Linear_model.score(X_test,y_test) 
-    print('Linear Model:')
-    #print('Intercept_:',Linear_model.intercept_)
-    #print('Coef:',Linear_model.coef_) 
-    print('Linear Model Accuracy:',acc)
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+
+    scaled_train_data = scaler.transform(X_train)
+    scaler_test_data = scaler.transform(X_test)
+
+
+
+    param_grid = {
+
+        'hidden_layer_sizes': [(10, ), (15, ), (20, ), (5, 5)],
+
+        'activation': ['logistic', 'tanh', 'relu'],
+
+        'alpha': [0.001, 0.01, 0.1, 0.2, 0.4, 1, 10]
+    }
+
+    mlp = MLPClassifier(max_iter=1000)
+
+    gcv = GridSearchCV(estimator=mlp, param_grid=param_grid, cv=4, n_jobs=-1)
+    gcv.fit(scaled_train_data, y_train)
     
-    y_pred = Linear_model.predict(X_test)
+    
+    mlp = MLPClassifier(hidden_layer_sizes=gcv.best_params_['hidden_layer_sizes'], activation=gcv.best_params_['activation'], alpha=gcv.best_params_['alpha'], max_iter=1000)
+
+    mlp.fit(scaled_train_data, y_train)
+
+    print(mlp)
+    
+    train_predict = mlp.predict(scaled_train_data)
+    test_predict = mlp.predict(scaler_test_data)
+
+
+    train_proba = mlp.predict_proba(scaled_train_data)
+    test_proba = mlp.predict_proba(scaler_test_data)    
+
+
+    print(metrics.confusion_matrix(y_test, test_predict))
+    print(metrics.classification_report(y_test, test_predict))  
+
+    print(mlp.score(scaler_test_data, y_test))
     
     plt.figure(facecolor='w')  
-    plt.plot(range(len(y_pred)),y_test,'r-',linewidth=2,label='test')
-    plt.plot(range(len(y_pred)),y_pred,'g-',linewidth=2,label='predict')
+    plt.plot(range(len(test_predict)),y_test,'r-',linewidth=2,label='test')
+    plt.plot(range(len(test_predict)),test_predict,'g-',linewidth=2,label='predict')
     plt.legend(loc='upper left') 
     plt.grid(True)  
     plt.show()
-  
-    print("Cross Validation:")
-    scores  = cross_val_score(Linear_model, X_test, y_test, cv=10).mean()
-    print("Cross_Val_Score: {}".format(scores))
-    
+
+
     
 
