f291f10fcb7bb46335d548d14eea9558341aa4be
Haolan-Ni <77455717+Haolan-Ni@users.noreply.github.com>
2021-03-18 19:42:18 +0800

    Update lib.py

--- Dwell/DwellML/DwellMl/Ml_models/lib.py
+++ Dwell/DwellML/DwellMl/Ml_models/lib.py
@@ -194,5 +194,62 @@ def KNN_Class_model(data):
     print("Cross_Val_Score: {}".format(scores))
     
 
+def KNN_Reg_model(data):
+    """ 
+    This function performs KNN_Reg_model on the loaded data.
+    Parameters
+    ----------
+    data: loaded data
+    Returns
+    -------
+    KNN_Reg_model scores
+    """
     
+    import lightgbm as lgb
+    from sklearn.metrics import mean_squared_error
+    from sklearn.datasets import load_iris
+    from sklearn.model_selection import train_test_split
+    import sklearn.linear_model as sk_linear
+    from sklearn.model_selection import cross_val_score
+    import matplotlib.pyplot as plt
+    from sklearn import neighbors
+    import warnings
+
+    warnings.filterwarnings('ignore')
+
+    df = data
+    df = df.fillna(0)
+    feature = ['Device_ID','Category_Num','Gender_Num','Geohash_Num','Start_time_Cat','End_time_Cat','Start_Time_Weight','End_Time_Weight','Duration','Weekday','Week_Num','Month_Num','tavg','weather']
+    X = df[feature]
+    y = df['Next_Geohash_Num']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    print("Train data length:", len(X_train))
+    print("Test data length:", len(X_test))
+    
+        
+
+    grid_search=GridSearchCV(knn_clf,param_grid,n_jobs=-1,verbose=2, cv = 10)
+    grid_search.fit(X_train,y_train)
+    
+
+    KNN_Reg_model = neighbors.KNeighborsRegressor(n_neighbors=grid_search.best_params_['n_neighbors'], p=grid_search.best_params_['p'], weights=grid_search.best_params_['weights']) 
+    KNN_Reg_model.fit(X_train,y_train)
+    acc8=KNN_Reg_model.score(X_test,y_test)    
+    
+    print('KNN_Reg_model:')
+    print('KNN_Reg_model Accuracy:',acc8)
+    
+    y_pred = KNN_Reg_model.predict(X_test)
+    
+    plt.figure(facecolor='w')  
+    plt.plot(range(len(y_pred)),y_test,'r-',linewidth=2,label='test')
+    plt.plot(range(len(y_pred)),y_pred,'g-',linewidth=2,label='predict')
+    plt.legend(loc='upper left') 
+    plt.grid(True)  
+    plt.show()
+  
+    print("Cross Validation:")
+    scores  = cross_val_score(KNN_Reg_model, X_test, y_test, cv=10).mean()
+    print("Cross_Val_Score: {}".format(scores))    
 
